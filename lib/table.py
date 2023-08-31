dfa6a041c478f815454abb9f8b95d864dfddcc13
Jak Crow <jak@jxcrw.io>
2023-08-31 07:20:23 -0700

    +lib: Add support for dynamic balls

--- lib/table.py
+++ lib/table.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Pool table"""
 
+import random
 from utils import BALLS, TABLE, diamond2pixel
 
 
@@ -10,16 +11,27 @@ class Table:
         self.table = TABLE.copy()
 
     def add_ball(self, ball: str, pos: (float, float)) -> None:
-        """Add a ball to the table at a given position."""
+        """Add a ball at the given position."""
         ball = BALLS[ball]
         pos_px = diamond2pixel(*pos)
         self.table.paste(ball, pos_px, mask=ball)
 
     def add_balls(self, balls: dict[str, list]) -> None:
-        """Add multiple balls to the table at the given positions."""
+        """Add multiple balls at the given positions."""
         for ball, positions in balls.items():
             for pos in positions:
                 self.add_ball(ball, pos)
+
+    def add_dyn_ball(self, dyn_ball: str, choices_x: list, choices_y: list) -> None:
+        """Add a dynamic ball at a random position chosen from the given choices."""
+        x = random.choice(choices_x)
+        y = random.choice(choices_y)
+        self.add_ball(dyn_ball, (x, y))
+
+    def add_dyn_balls(self, dyn_balls: dict[str, tuple[list]]) -> None:
+        """Add multiple dynamic balls at random positions."""
+        for dyn_ball, choices in dyn_balls.items():
+            self.add_dyn_ball(dyn_ball, *choices)
 
     def show(self) -> None:
         """Display the table."""
